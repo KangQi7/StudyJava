f77760ff320cd15692c3b00a17f8afbd637142a6
F-00253 <keruI2018Kq>
2019-12-25 08:52:24 +0800

    全提交

--- src/main/java/genericity/TestGenericity.java
+++ src/main/java/genericity/TestGenericity.java
@@ -4,9 +4,11 @@ import jsoup.test.skuQuestion.JdSkuQuestionModel;
 import jsoup.test.skuQuestion.SkuInfo;
 import jsoup.test.skuQuestion.SkuQuestion;
 import org.junit.Test;
+import sun.security.krb5.internal.ktab.KeyTabInputStream;
 
 import javax.lang.model.type.ReferenceType;
 import java.lang.reflect.*;
+import java.math.BigDecimal;
 import java.util.*;
 
 /**
@@ -20,19 +22,22 @@ public class TestGenericity {
      * 敏感字段
      */
     private static final List<String> SENSITIVE_FIELDS = Arrays.asList("expire",
-            "encrypt_number", "resultCode", "questionList", "customerName","brandName"
+            "encrypt_number", "resultCode", "questionList", "customerName", "brandName"
     );
-    //
-    private static List<Type> notTypes = Arrays.asList(String.class,Integer.class, Long.class, Date.class,
-            Map.class, HashMap.class,Dictionary.class,
-            int.class, long.class, float.class, double.class, boolean.class, byte.class,char.class,short.class);
+    /**
+     * 不处理类型
+     */
+    private static List<Type> notTypes = Arrays.asList(String.class, Integer.class, Long.class, Date.class,
+            Map.class, Dictionary.class, BigDecimal.class, Double.class, Float.class, Boolean.class,//HashMap.class
+            Byte.class, Short.class,
+            int.class, long.class, float.class, double.class, boolean.class, byte.class, char.class, short.class);
 
-    private static String decrypt(String token, String text){
+    private static String decrypt(String token, String text) {
         text = text.replaceAll("&" + token, "");
         return text;
     }
 
-    private static String encrypt(String token,String text){
+    private static String encrypt(String token, String text) {
         text += "&" + token;
         return text;
     }
@@ -74,107 +79,25 @@ public class TestGenericity {
                 .encrypt_number("2111")
                 .build();
 
-        List<JdSkuQuestionModel> models = Arrays.asList(model,model2);
+        List<JdSkuQuestionModel> models = Arrays.asList(model, model2);
 
-        encryptObject(models, token);
+        List<Map> maps = new ArrayList<>();
+        HashMap map1 = new HashMap();
+        map1.put("brandName","name1");
+        map1.put("resultCode",1);
 
-        decryptObject(models, token);
+        HashMap map2 = new HashMap();
+//        map2.put("brandName","name2");
+//        map2.put("resultCode",2);
+        maps.add(map1);
+        maps.add(map2);
+
+        encryptObject(maps, token);
+
+        decryptObject(maps, token);
 
         System.out.println(model.getQuestionList());
     }
-
-    public static <T> void decryptObj(T obj, String token){
-        if (obj instanceof List<?>) {
-            for (Object o : (List<?>) obj) {
-//                decryptObject(o , token);
-            }
-        }else {
-//            decryptObject(obj, token);
-        }
-    }
-
-//    public static <T> void decryptObject(T obj, String token) {
-//        if(obj == null) {
-//            return;
-//        }
-//        //如果是数组类型，循环执行
-//        if (obj instanceof List<?>) {
-//            for (Object o : (List<?>) obj) {
-//                decryptObject(o , token);
-//            }
-//        } else {
-//            //1.判断obj类型，如果是无需处理的类型则直接返回
-//            if (types.contains(obj.getClass())){
-//                return;
-//            }
-//
-//            //2.获取类型各字段进行操作
-//            Field[] fields = obj.getClass().getDeclaredFields();
-//
-//            for (Field field : fields) {
-//                //如果是数组，获取其类型并循环递归
-//                if (field.getGenericType() instanceof ParameterizedType) {
-//                    ParameterizedType pt = (ParameterizedType) field.getGenericType();
-//                    //数组，且其类型不为基本类型、String或Integer等无需处理字段的类型
-//                    if (pt.getRawType().equals(List.class) && !types.contains(pt.getActualTypeArguments()[0])) {
-//                        try {
-//                            field.setAccessible(true);
-//                            decryptObject(field.get(obj), token);
-//                        } catch (IllegalAccessException e) {
-//                            e.printStackTrace();
-//                        }
-//                    } else if(pt.getRawType().equals(List.class)
-//                            && pt.getActualTypeArguments()[0].equals(String.class)
-//                            && SENSITIVE_FIELDS.contains(field.getName())){
-//                        try {
-//                            field.setAccessible(true);
-//                            List objList = (List)field.get(obj);
-//                            List newList = new ArrayList();
-//                            for (Object item:
-//                                    objList) {
-//                                item += "1";
-//                                newList.add(item);
-//                            }
-//                            field.set(obj, newList);
-//                        } catch (IllegalAccessException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                } else {
-//                    //String类型，判断是否是加密字段，是则解密并赋值
-//                    if (field.getGenericType().equals(String.class)) {
-//                        if (!SENSITIVE_FIELDS.contains(field.getName())) {
-//                            continue;//不是则继续循环字段
-//                        }
-//
-//                        try {
-//                            field.setAccessible(true);
-//                            String value = field.get(obj).toString() + "1";
-//                            field.set(obj, value);
-//                            //处理加密字段
-//                            if (field.getName().startsWith("encrypt_")) {
-//                                Field _field = obj.getClass().getDeclaredField(field.getName().replace("encrypt_", ""));
-//                                _field.setAccessible(true);
-//                                _field.set(obj, Long.parseLong(value));
-//                            }
-//                        } catch (IllegalAccessException | NoSuchFieldException e) {
-//                            e.printStackTrace();
-//                        }
-//                    } else if(!types.contains(field.getType())) {
-//                        try {
-//                            field.setAccessible(true);
-//                            decryptObject(field.get(obj), token);
-//                        } catch (IllegalAccessException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-
-
 
     /**
      * 解密object中的敏感字段
@@ -189,10 +112,24 @@ public class TestGenericity {
         }
         //如果是数组类型，循环执行
         if (obj instanceof List<?>) {
-            if(notTypes.contains(((List<?>) obj).get(0).getClass()))
+            //如果数组无元素，直接返回
+            if (((List<?>) obj).size() == 0)
+                return;
+
+            if (notTypes.contains(((List<?>) obj).get(0).getClass()))
                 return;
             for (Object o : (List<?>) obj) {
                 decryptObject(o, token);
+            }
+        } else if (obj instanceof Map){
+            Iterator keys = ((Map) obj).keySet().iterator();
+            while (keys.hasNext()){
+                String key = keys.next().toString();
+                if (SENSITIVE_FIELDS.contains(key)){
+                    String value = ((Map) obj).get(key).toString();
+                    value = decrypt(token, value);
+                    ((Map) obj).put(key, value);
+                }
             }
         } else {
             //1.判断obj类型，如果是无需处理的类型则直接返回
@@ -204,68 +141,38 @@ public class TestGenericity {
             Field[] fields = obj.getClass().getDeclaredFields();
 
             for (Field field : fields) {
-//                //如果是数组，获取其类型并循环递归
-//                if (field.getGenericType() instanceof ParameterizedType) {
-//                    ParameterizedType pt = (ParameterizedType) field.getGenericType();
-//                    //数组，且其类型不为基本类型、String或Integer等无需处理字段的类型
-//                    if (pt.getRawType().equals(List.class) && !notTypes.contains(pt.getActualTypeArguments()[0])) {
-//                        try {
-//                            field.setAccessible(true);
-//                            decryptObject(field.get(obj), token);
-//                        } catch (IllegalAccessException e) {
-//                            e.printStackTrace();
-//                        }
-//                    } else if (pt.getRawType().equals(List.class)
-//                            && pt.getActualTypeArguments()[0].equals(String.class)
-//                            && SENSITIVE_FIELDS.contains(field.getName())) {
-//                        try {
-//                            field.setAccessible(true);
-//                            List objList = (List) field.get(obj);
-//                            List newList = new ArrayList();
-//                            for (Object item :
-//                                    objList) {
-//                                String value = decrypt(token, item.toString());//解密操作
-//                                newList.add(value);
-//                            }
-//                            field.set(obj, newList);
-//                        } catch (IllegalAccessException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                } else {
-                    //String类型，判断是否是加密字段，是则解密并赋值
-                    if (field.getGenericType().equals(String.class)) {
-                        if (!SENSITIVE_FIELDS.contains(field.getName())) {
-                            continue;//不是则继续循环字段
-                        }
+                //String类型，判断是否是加密字段，是则解密并赋值
+                if (field.getGenericType().equals(String.class)) {
+                    if (!SENSITIVE_FIELDS.contains(field.getName())) {
+                        continue;//不是则继续循环字段
+                    }
 
-                        try {
-                            field.setAccessible(true);
-                            Object enValue = field.get(obj);//这里可能因字段值为null而报错，所以进行了非空判断
-                            if (enValue != null) {
-                                String value = decrypt(token, enValue.toString());//解密操作
-                                field.set(obj, value);
-                                //处理加密字段
-                                if (field.getName().startsWith("encrypt_")) {
-                                    Field _field = obj.getClass().getDeclaredField(field.getName().replace("encrypt_", ""));
-                                    _field.setAccessible(true);
-                                    _field.set(obj, Long.parseLong(value));
-                                }
+                    try {
+                        field.setAccessible(true);
+                        Object enValue = field.get(obj);//这里可能因字段值为null而报错，所以进行了非空判断
+                        if (enValue != null) {
+                            String value = decrypt(token, enValue.toString());//解密操作
+                            field.set(obj, value);
+                            //处理加密字段
+                            if (field.getName().startsWith("encrypt_")) {
+                                Field _field = obj.getClass().getDeclaredField(field.getName().replace("encrypt_", ""));
+                                _field.setAccessible(true);
+                                _field.set(obj, Long.parseLong(value));
                             }
-                        } catch (IllegalAccessException | NoSuchFieldException e) {
-                            e.printStackTrace();
                         }
-                    } else if (!notTypes.contains(field.getType())) {
-                        try {
-                            field.setAccessible(true);
-                            decryptObject(field.get(obj), token);
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                        }
+                    } catch (IllegalAccessException | NoSuchFieldException e) {
+                        e.printStackTrace();
+                    }
+                } else if (!notTypes.contains(field.getType())) {
+                    try {
+                        field.setAccessible(true);
+                        decryptObject(field.get(obj), token);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
                     }
                 }
             }
-//        }
+        }
     }
 
     /**
@@ -281,10 +188,24 @@ public class TestGenericity {
         }
         //如果是数组类型，循环执行
         if (obj instanceof List<?>) {
-            if(notTypes.contains(((List<?>) obj).get(0).getClass()))
+            //如果数组无元素，直接返回
+            if (((List<?>) obj).size() == 0)
+                return;
+
+            if (notTypes.contains(((List<?>) obj).get(0).getClass()))
                 return;
             for (Object o : (List<?>) obj) {
                 encryptObject(o, token);
+            }
+        } else if (obj instanceof Map){
+            Iterator keys = ((Map) obj).keySet().iterator();
+            while (keys.hasNext()){
+                String key = keys.next().toString();
+                if (SENSITIVE_FIELDS.contains(key)){
+                    String value = ((Map) obj).get(key).toString();
+                    value = encrypt(token, value);
+                    ((Map) obj).put(key, value);
+                }
             }
         } else {
             //1.判断obj类型，如果是无需处理的类型则直接返回
@@ -296,76 +217,47 @@ public class TestGenericity {
             Field[] fields = obj.getClass().getDeclaredFields();
 
             for (Field field : fields) {
-//                //如果是数组，获取其类型并循环递归
-//                if (field.getGenericType() instanceof ParameterizedType) {
-//                    ParameterizedType pt = (ParameterizedType) field.getGenericType();
-//                    //数组，且其类型不为基本类型、String或Integer等无需处理字段的类型
-//                    if (pt.getRawType().equals(List.class) && !notTypes.contains(pt.getActualTypeArguments()[0])) {
-//                        try {
-//                            field.setAccessible(true);
-//                            encryptObject(field.get(obj), token);
-//                        } catch (IllegalAccessException e) {
-//                            e.printStackTrace();
-//                        }
-//                    } else if (pt.getRawType().equals(List.class)
-//                            && pt.getActualTypeArguments()[0].equals(String.class)
-//                            && SENSITIVE_FIELDS.contains(field.getName())) {
-//                        try {
-//                            field.setAccessible(true);
-//                            List objList = (List) field.get(obj);
-//                            List newList = new ArrayList();
-//                            for (Object item :
-//                                    objList) {
-//                                String value = encrypt(token, item.toString());//加密操作
-//                                newList.add(value);
-//                            }
-//                            field.set(obj, newList);
-//                        } catch (IllegalAccessException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                } else {
-                    //String类型，判断是否是加密字段，是则解密并赋值
-                    if (field.getGenericType().equals(String.class) ||
-                            field.getGenericType().equals(Long.class) ||
-                            field.getGenericType().equals(Integer.class)) {
-                        if (!SENSITIVE_FIELDS.contains(field.getName())) {
-                            continue;//不是则继续循环字段
-                        }
+                //String类型，判断是否是加密字段，是则解密并赋值
+                if (field.getGenericType().equals(String.class) ||
+                        field.getGenericType().equals(Long.class) ||
+                        field.getGenericType().equals(Integer.class)) {
+                    if (!SENSITIVE_FIELDS.contains(field.getName())) {
+                        continue;//不是则继续循环字段
+                    }
 
-                        try {
-                            field.setAccessible(true);
-                            Object deValue = field.get(obj);//这里可能因字段值为null而报错，所以进行了非空判断
-                            if (deValue != null) {
-                                String value = encrypt(token, deValue.toString());//加密操作
-                                //如果字段类型是整型、长整型，将密文赋值给其对应的加密字段
-                                if (field.getGenericType().equals(Long.class) ||
-                                        field.getGenericType().equals(Integer.class)) {
-                                    //处理加密字段，判断加密字段存在
-                                    Field _field = obj.getClass().getDeclaredField("encrypt_" + field.getName());
-                                    if (_field != null) {
-                                        _field.setAccessible(true);
-                                        _field.set(obj, value);
-                                    }
-                                    //赋值敏感字段为null
-                                    field.set(obj, null);
-                                }else {
-                                    field.set(obj, value);
+                    try {
+                        field.setAccessible(true);
+                        Object deValue = field.get(obj);//这里可能因字段值为null而报错，所以进行了非空判断
+                        if (deValue != null) {
+                            String value = encrypt(token, deValue.toString());//加密操作
+                            //如果字段类型是整型、长整型，将密文赋值给其对应的加密字段
+                            if (field.getGenericType().equals(Long.class) ||
+                                    field.getGenericType().equals(Integer.class)) {
+                                //处理加密字段，判断加密字段存在
+                                Field _field = obj.getClass().getDeclaredField("encrypt_" + field.getName());
+                                if (_field != null) {
+                                    _field.setAccessible(true);
+                                    _field.set(obj, value);
                                 }
+                                //赋值敏感字段为null
+                                field.set(obj, null);
+                            } else {
+                                field.set(obj, value);
                             }
-                        } catch (IllegalAccessException | NoSuchFieldException e) {
-                            e.printStackTrace();
                         }
-                    } else if (!notTypes.contains(field.getType())) {
-                        try {
-                            field.setAccessible(true);
-                            encryptObject(field.get(obj), token);
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                        }
+                    } catch (IllegalAccessException | NoSuchFieldException e) {
+                        e.printStackTrace();
+                    }
+                } else if (!notTypes.contains(field.getType())) {
+                    try {
+                        field.setAccessible(true);
+                        encryptObject(field.get(obj), token);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
                     }
                 }
             }
+        }
 //        }
     }
 }
